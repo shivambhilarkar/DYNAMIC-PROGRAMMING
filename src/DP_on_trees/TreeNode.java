3e05a4b4ea65fb31fa39850fd54e93110a2e2511
Shivam Bhilarkar <shivambhilarkar@gmail.com>
2024-03-28 23:02:37 +0530

    .

---
+++ src/DP_on_trees/TreeNode.java
@@ -0,0 +1,14 @@
+package DP_on_trees;
+
+public class TreeNode {
+
+    int value;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode(int value) {
+        this.value = value;
+        this.left = null;
+        this.right = null;
+    }
+}
